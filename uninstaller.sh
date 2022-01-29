eea276143e72399f6d6821b7380354894d748baf
UVP107 <98437921+UVP107@users.noreply.github.com>
2022-01-29 15:06:20 +0700

    Add files via upload

---
+++ uninstaller.sh
@@ -0,0 +1,4 @@
+echo "Memulai penghapusan"
+apt purge mariadb-client mariadb-server bind9 apache2 php-mysql php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip links -y
+rm -rf /etc/apache2/ /etc/bind/ /etc/mysql/ /var/cache/bind9/ /var/www/html
+apt autoremove -y
